e502b3d98dac8c796edf6bb147dfa0430d622360
shamusdougan <shamus@douganfamily.com.au>
2015-03-08 16:20:25 +1100

    Improved basic template
    Basic template only no changes

---
+++ _protected/config/db.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=sapient_imp',
+    'username' => 'impadmin',
+    'password' => 's@p13nt',
+    'charset' => 'utf8',
+];
