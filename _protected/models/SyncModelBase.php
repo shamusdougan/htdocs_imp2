c2b054b50709ed4a6bf6d72611fe76e4661bc8d5
shamusdougan <shamus@douganfamily.com.au>
2015-06-02 16:30:00 +1000

    sync model changes

--- _protected/models/SyncModelBase.php
+++ _protected/models/SyncModelBase.php
@@ -32,7 +32,14 @@ Class syncModelBase{
 		
 		if($syncRelationship->endPointType == Syncrelationships::ENDPOINTTYPE_DATABASE)
 		{
-			return "Database Sync";
+			$dataConnection = $this->connectDatabase($syncRelationship);
+			if(is_string($dataConnection))
+				{
+				return $dataConnection;
+				}
+			else{
+				return $this->syncDatabase($dataConnection, $syncRelationship);
+			}
 		}
 		elseif($syncRelationship->endPointType == Syncrelationships::ENDPOINTTYPE_WEB)
 		{
@@ -46,6 +53,67 @@ Class syncModelBase{
 		
 	}
 	
+	/*
+	Function connectDatabase
+	Descitpion: takes the syncRelationship object and connected to the database
+	inputs: syncRelationship Object -> see syncRelationship Model
+	outputs: either the database connections object or the error message to be returned
+	*/
+	function connectDatabase($syncRelationship)
+	{
+		$dsn = "mysql:host=".$syncRelationship->endPointDBServer.";dbname=".$syncRelationship->endPointDBName;
+		$connection = new \yii\db\Connection([
+		    'dsn' => $dsn,
+		    'username' => $syncRelationship->endPointUser,
+		    'password' => $syncRelationship->endPointPassword,
+		]);
+	
+	try{
+		$connection->open();	
+		}
+	catch(Exception $e)
+		{
+			return "Unable to connect to Database: ".$dsn." using: ".$syncRelationship->endPointUser."\nError Message Returned: ".$e->getMessage();
+		}
+	
+	return $connection;
+	
+	}
+	
+	
+	/*
+	Function: syncDatabase
+	Description: Takes the db Connection obejcts and performs the sync, this is designed to be run from the child object that 
+	input: connection -> the yii connction object for the foriegn database
+		$syncRelationship -> the given sync relationship object. This object stores the information for the last sync etc
+	
+	*/
+	function syncDatabase($connection, $syncRelationship)
+	{
+		//query the local database for any records that have changed since the last sync
+		
+		
+		//$results = $connection->createCommand("Select * From clients")->queryAll();
+		if($syncRelationship->lastSync == "")
+		{
+			
+			$syncRelationship->lastSync = date("Y-m-d H:i:s");
+			$syncRelationship->save();
+			return $syncRelationship->lastSync;
+			
+			
+			return "No Previous sync performed";
+		}
+		else
+		{
+			$syncRelationship->lastSync = date("Y-m-d H:i:s");
+			$syncRelationship->save();
+			return $syncRelationship->lastSync;
+		}
+		
+		
+		
+	}
 	
 }
 
